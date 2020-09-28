2137148ecb782005d08d4d81a5b9ad4ebc6c7d42
Alexander <59923093+ucle7am@users.noreply.github.com>
2020-09-28 18:11:46 +0300

    feat: add styles co movie cards

--- src/components/Aplication/Main/Main.jsx
+++ src/components/Aplication/Main/Main.jsx
@@ -1,11 +1,51 @@
 import React from "react";
+import styles from "./Main.module.css";
+import imdbLogo from "../../../assets/imdb.png";
+import pgLogo from "../../../assets/PG.png";
 
 const Main = (props) => {
   console.log(props);
   const movieCards = props.movieArr.map((el) => {
     return (
-      <div>
-        <p>{el.Title}</p>
+      <div className={styles.movie}>
+        <div>
+          <img className={styles.poster} src={el.Poster} alt="no poster" />
+        </div>
+        <div className={styles.movieCard}>
+          <p className={styles.name}>{el.Title}</p>
+          <span className={styles.year}>{el.Year}</span>
+          <span className={styles.runtime}>{el.Runtime}</span>
+          <span className={styles.genre}>{el.Genre}</span>
+          <p className={styles.plot}>{el.Plot}</p>
+          <p className={styles.rating}>
+            Rating{" "}
+            <span
+              className={
+                el.imdbRating > 6 ? styles.ratingGreen : styles.ratingYellow
+              }
+            >
+              {el.imdbRating}
+            </span>
+          </p>
+          <div className={styles.trailer}>
+            <a
+              className={styles.trailerLink}
+              target="_blank"
+              href={`https://www.youtube.com/results?search_query=${el.Title}+${el.Year}+trailer`}
+            >
+              Watch Trailer
+            </a>
+          </div>
+          <div className={styles.watch}>
+            <a
+              className={styles.watchLink}
+              target="_blank"
+              href={`https://www.imdb.com/title/${el.imdbID}`}
+            >
+              Watch Movie
+            </a>
+          </div>
+        </div>
       </div>
     );
   });
