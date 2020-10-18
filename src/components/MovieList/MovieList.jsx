6b3c9ae6e5510f485987e8f1fcbf0f376f1da3be
Alexander <59923093+ucle7am@users.noreply.github.com>
2020-10-19 02:14:55 +0300

    fix: delete unUsed

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "./MovieList.module.css";
 import MovieCard from "../MovieCard/MovieCard";
 const MovieList = ({ movieArr, setPopUpImg }) => {
   const movieCards = movieArr.map((el) => (
