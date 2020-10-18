1763910b4a15570625c890712b5092008bd4af97
Alexander <59923093+ucle7am@users.noreply.github.com>
2020-10-19 01:32:45 +0300

    feat: add shadow to popUp

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,17 @@ import SearchBar from "./components/SearchBar/SearchBar";
 
 const App = ({ movieArr, ...props }) => {
   return (
-    <div className="container">
-      <SearchBar {...props} />
-      <Main movieArr={movieArr} {...props} />
-      <div
-        style={/*props.popUpImg.length*/ 1 > 0 ? {} : { display: "none" }}
-        className="popUp"
-      ></div>
-    </div>
+    <>
+      <div className="container">
+        <SearchBar {...props} />
+        <Main movieArr={movieArr} {...props} />
+        <div
+          style={/*props.popUpImg.length*/ 1 > 0 ? {} : { display: "none" }}
+          className="popUp"
+        ></div>
+      </div>
+      <div className="shadow"></div>
+    </>
   );
 };
 
