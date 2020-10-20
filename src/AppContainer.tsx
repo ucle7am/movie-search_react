a503929722b34866621766d006fd7b97d26c0693
Alexander <59923093+ucle7am@users.noreply.github.com>
2020-10-20 18:11:35 +0300

    feat: add TS to app container

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -11,11 +11,21 @@ import {
   getNewMoviesThunkCreator,
 } from "./store/thunks";
 import App from "./App";
-const mapStateToProps = (state) => {
+interface stateToPros {
+  inputValue:string,
+  isFetching:boolean,
+  movieArr: Array<object>,
+  page: number,
+  totalPages: number,
+  hasResponse: string,
+  error: string,
+  currentSearch: string,
+  popUpImg: string,
+}
+const mapStateToProps = (state:stateToPros) => {
   return {
     inputValue: state.inputValue,
     isFetching: state.isFetching,
-    isEnglish: state.isEnglish,
     movieArr: state.movieArr,
     page: state.page,
     totalPages: state.totalPages,
@@ -25,27 +35,27 @@ const mapStateToProps = (state) => {
     popUpImg: state.popUpImg,
   };
 };
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch:Function) => {
   return {
-    changeInput: (text) => {
+    changeInput: (text:string) => {
       dispatch(changeInputAC(text));
     },
     reset: () => {
       dispatch(resetAC());
     },
-    setPage: (page) => {
+    setPage: (page: number) => {
       dispatch(setPageAC(page));
     },
-    getMovies: (movie, page) => {
+    getMovies: (movie:string, page:number) => {
       dispatch(getMoviesThunkCreator(movie, page));
     },
-    getNewMovies: (movie) => {
+    getNewMovies: (movie:string) => {
       dispatch(getNewMoviesThunkCreator(movie));
     },
-    setCurrentSearch: (value) => {
+    setCurrentSearch: (value:number) => {
       dispatch(setCurrentSearchAC(value));
     },
-    setPopUpImg: (src) => {
+    setPopUpImg: (src:string) => {
       dispatch(setPopUpImgAC(src));
     },
   };
