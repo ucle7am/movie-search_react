95bd7fb52a6a2fb82a6fea76178a1eccb000fea4
Alexander <59923093+ucle7am@users.noreply.github.com>
2020-10-22 11:45:10 +0300

    feat: add check of status code of request

--- src/service/ajaxRequests.js
+++ src/service/ajaxRequests.js
@@ -2,13 +2,22 @@ const API_KEY = "b2dcd879";
 export async function getMovies(movie, page) {
   const url = `https://www.omdbapi.com/?s=${movie}&page=${page}&apikey=${API_KEY}`;
   const res = await fetch(url);
-  const data = await res.json();
-  return data;
+  if (res.status === 200) {
+    const data = await res.json();
+    return data;
+  }else{
+    return 'bad request'
+  }
+  
 }
 export async function getMovieById(id) {
   const url = `https://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`;
   const res = await fetch(url);
-  const data = await res.json();
-  return data;
+  if (res.status === 200) {
+    const data = await res.json();
+    return data;
+  }else{
+    return 'bad request'
+  }
 }
 export const fillArrayMoviesId = (arr) => arr.map((el) => el.imdbID);
--- src/store/thunks.js
+++ src/store/thunks.js
@@ -15,14 +15,16 @@ export const getMoviesThunkCreator = (movie, page) => {
   return async (dispatch) => {
     dispatch(toggleFecthAC(true));
     const movies = await getMovies(movie, page);
-    if (movies.Response === "True") {
+    if (movies !== 'bad request' && movies.Response === "True") {
       dispatch(setResponseAC(true, ""));
       dispatch(setTotalPagesAC(+movies.totalResults));
       const promiseArray = fillArrayMoviesId(movies.Search).map((el) => getMovieById(el));
       const promiseArrayResolvedMovies = await Promise.all(promiseArray);
       dispatch(addMoviesToStateAC(promiseArrayResolvedMovies));
-    }else {
+    }else if(movies !== 'bad request' && movies.Response === "False"){
       dispatch(setResponseAC(false, movies.Error));
+    }else if(movies === 'bad request'){
+      dispatch(setResponseAC(false, movies));
     }
     dispatch(toggleFecthAC(false));
   };
